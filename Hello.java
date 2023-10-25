ffebc6a34419f10122034909524971a57110bc8c
ehdwls4677 <ehdwls4677@naver.com>
2023-10-25 17:36:06 +0900

    hello java

---
+++ Hello.java
@@ -0,0 +1,15 @@
+package day01;
+
+public class Hello {
+	
+	public static void main(String[] args) {
+		
+		System.out.print ("hello world\n");
+		System.out.print ("헬로 월드\n");
+		
+		System.out.print("1\n");
+		System.out.print(1);
+	}
+
+}
+
